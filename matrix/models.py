c76d055aaa80e48d55133442e42fc8029c39e0ee
Kevin Coakley <kcoakley@sdsc.edu>
2022-11-22 20:39:34 -0800

    Added the first version for the fair matrix
    Signed-off-by: Kevin Coakley <kcoakley@sdsc.edu>

--- matrix/models.py
+++ matrix/models.py
@@ -2,6 +2,115 @@ from django.db import models
 
 
 class Survey(models.Model):
-    q01_products = models.CharField(max_length=256)
-    q02_project_name = models.CharField(max_length=256)
-    time_stamp = models.DateField(auto_now_add=True)
+    q01_products = models.CharField(max_length=256, default="", blank=True)
+    q02_project_name = models.CharField(max_length=256, default="", blank=True)
+    q05_products_posted = models.CharField(max_length=256, default="", blank=True)
+    q05_products_posted_other = models.CharField(max_length=256, default="", blank=True)
+    q06_simulations = models.CharField(max_length=256, default="", blank=True)
+    q07_license = models.CharField(max_length=256, default="", blank=True)
+    q08_license_other = models.CharField(max_length=256, default="", blank=True)
+    q08_license_other_institutional = models.CharField(
+        max_length=256, default="", blank=True
+    )
+    q08_license_other_none = models.CharField(max_length=256, default="", blank=True)
+    q09_open_data = models.CharField(max_length=256, default="", blank=True)
+    q10_indigenous_populations = models.CharField(
+        max_length=256, default="", blank=True
+    )
+    q11_specialized_software_open_source = models.CharField(
+        max_length=256, default="", blank=True
+    )
+    q12_specialized_software = models.CharField(max_length=256, default="", blank=True)
+    q12_specialized_software_specify = models.CharField(
+        max_length=256, default="", blank=True
+    )
+    q13_existing_libraries = models.CharField(max_length=256, default="", blank=True)
+    q14_nano_publications = models.CharField(max_length=256, default="", blank=True)
+    q15_data_formats_csv = models.CharField(max_length=256, default="", blank=True)
+    q15_data_formats_database = models.CharField(max_length=256, default="", blank=True)
+    q15_data_formats_hd5f = models.CharField(max_length=256, default="", blank=True)
+    q15_data_formats_json = models.CharField(max_length=256, default="", blank=True)
+    q15_data_formats_other = models.CharField(max_length=256, default="", blank=True)
+    q15_data_formats_other_value = models.CharField(
+        max_length=256, default="", blank=True
+    )
+    q15_data_formats_spreadsheet = models.CharField(
+        max_length=256, default="", blank=True
+    )
+    time_stamp = models.DateTimeField(auto_now_add=True)
+
+    def q01_products_list(self):
+        return [x.strip() for x in self.q01_products.split(",")]
+
+    def q05_products_posted_list(self):
+        products_posted = [x.strip() for x in self.q05_products_posted.split(",")]
+
+        if "Other (Specify)".casefold() in map(str.casefold, products_posted):
+            products_posted.remove("Other (Specify)")
+            products_posted.append(self.q05_products_posted_other)
+
+        return products_posted
+
+    def q15_data_formats_csv_list(self):
+        return [x.strip() for x in self.q15_data_formats_csv.split(",")]
+
+    def q15_data_formats_database_list(self):
+        return [x.strip() for x in self.q15_data_formats_database.split(",")]
+
+    def q15_data_formats_hd5f_list(self):
+        return [x.strip() for x in self.q15_data_formats_hd5f.split(",")]
+
+    def q15_data_formats_json_list(self):
+        return [x.strip() for x in self.q15_data_formats_json.split(",")]
+
+    def q15_data_formats_other_list(self):
+        return [x.strip() for x in self.q15_data_formats_other.split(",")]
+
+    def q15_data_formats_spreadsheet_list(self):
+        return [x.strip() for x in self.q15_data_formats_spreadsheet.split(",")]
+
+    def q15_input_data_list(self):
+
+        input = []
+
+        if "input".casefold() in map(str.casefold, self.q15_data_formats_csv_list()):
+            input.append("CSV")
+        if "input".casefold() in map(
+            str.casefold, self.q15_data_formats_database_list()
+        ):
+            input.append("Database")
+        if "input".casefold() in map(str.casefold, self.q15_data_formats_hd5f_list()):
+            input.append("HDF5")
+        if "input".casefold() in map(str.casefold, self.q15_data_formats_json_list()):
+            input.append("JSON")
+        if "input".casefold() in map(
+            str.casefold, self.q15_data_formats_spreadsheet_list()
+        ):
+            input.append("Spreadsheet")
+        if "input".casefold() in map(str.casefold, self.q15_data_formats_other_list()):
+            input.append(self.q15_data_formats_other_value)
+
+        return input
+
+    def q15_output_data_list(self):
+
+        output = []
+
+        if "output".casefold() in map(str.casefold, self.q15_data_formats_csv_list()):
+            output.append("CSV")
+        if "output".casefold() in map(
+            str.casefold, self.q15_data_formats_database_list()
+        ):
+            output.append("Database")
+        if "output".casefold() in map(str.casefold, self.q15_data_formats_hd5f_list()):
+            output.append("HDF5")
+        if "output".casefold() in map(str.casefold, self.q15_data_formats_json_list()):
+            output.append("JSON")
+        if "output".casefold() in map(
+            str.casefold, self.q15_data_formats_spreadsheet_list()
+        ):
+            output.append("Spreadsheet")
+        if "input".casefold() in map(str.casefold, self.q15_data_formats_other_list()):
+            output.append(self.q15_data_formats_other_value)
+
+        return output
--- matrix/serializers.py
+++ matrix/serializers.py
@@ -5,4 +5,29 @@ from .models import Survey
 class SurveySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Survey
-        fields = ["q01_products", "q02_project_name", "time_stamp"]
+        fields = [
+            "q01_products",
+            "q02_project_name",
+            "q05_products_posted",
+            "q05_products_posted_other",
+            "q06_simulations",
+            "q07_license",
+            "q08_license_other",
+            "q08_license_other_institutional",
+            "q08_license_other_none",
+            "q09_open_data",
+            "q10_indigenous_populations",
+            "q11_specialized_software_open_source",
+            "q12_specialized_software",
+            "q12_specialized_software_specify",
+            "q13_existing_libraries",
+            "q14_nano_publications",
+            "q15_data_formats_csv",
+            "q15_data_formats_database",
+            "q15_data_formats_hd5f",
+            "q15_data_formats_json",
+            "q15_data_formats_other",
+            "q15_data_formats_other_value",
+            "q15_data_formats_spreadsheet",
+            "time_stamp",
+        ]

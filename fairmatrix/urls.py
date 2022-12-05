a779fe76d51b9ceced9145cb4d0d97265087e690
Kevin Coakley <kcoakley@sdsc.edu>
2022-12-05 13:37:07 -0800

    Redirect “/“ to settings.ROOT_REDIRECT_URL
    Signed-off-by: Kevin Coakley <kcoakley@sdsc.edu>

--- fairmatrix/urls.py
+++ fairmatrix/urls.py
@@ -13,10 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path(
+        "",
+        RedirectView.as_view(url=settings.ROOT_REDIRECT_URL),
+    ),
     path("matrix/", include("matrix.urls")),
     path("admin/", admin.site.urls),
 ]
